56b56a9c3045caf8c98cce07905d68f1cd573802
bfrosik <bfrosik@anl.gov>
2021-10-14 12:24:54 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+      name='pycohere',
+      author = 'Barbara Frosik, Ross Harder',
+      author_email = 'bfrosik@anl.gov',
+      url='https://github.com/advancedPhotonSource/pycohere',
+      version='1.0',
+      packages=setuptools.find_packages()
+)
