1600047c05dfbcba5bb9bdc9e9a76b609d78ae88
yudongyao <yaoyud1990@gmail.com>
2021-11-07 19:03:40 -0600

    use cohere.utilities.utils for preprocessing

--- cohere/controller/AI_guess.py
+++ cohere/controller/AI_guess.py
@@ -11,51 +11,6 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model,load_model
 from tensorflow.keras.activations import sigmoid, tanh
 
-def run_AI(data, threshold, sigma, dir):
-    print('AI guess')
-
-    print('original data shape', data.shape)
-    # prepare data to make the oversampling ratio ~3
-    wos = 3.0
-    orig_os = get_oversample_ratio(data)
-    # match oversampling to wos
-    wanted_os = [wos, wos, wos]
-    # match diff os
-    new_data, inshape = match_oversample_diff(data, orig_os, wanted_os)
-    print('processed data shape', new_data.shape)
-    new_data = new_data[np.newaxis]
-
-    # load trained network
-    model = load_model('trained_model.hdf5',custom_objects={'loss_comb2_scale': loss_comb2_scale})
-    print('successfully load the model')
-
-    # get the outputs from amplitude and phase layers
-    amp_layer_model = Model(inputs=model.input,
-                            outputs=model.get_layer('amp').output)
-    ph_layer_model = Model(inputs=model.input,
-                        outputs=model.get_layer('phi').output)
-
-    preds_amp = amp_layer_model.predict(new_data, verbose=1)
-
-    preds_phi = ph_layer_model.predict(new_data, verbose=1)
-
-    preds_amp, preds_phi = post_process(preds_amp[0,...,0],
-                                    preds_phi[0,...,0],
-                                    th=0.1,
-                                    uw=0)
-
-    pred_obj = preds_amp * np.exp(1j * preds_phi)
-    
-    # match object size with the input data
-    pred_obj = Resize(pred_obj, inshape)
-    guess = crop_pad(pred_obj, data.shape)
-    print('initial guess shape', guess.shape)
-
-    np.save(os.path.join(dir, 'image.npy'), guess)
-
-    support = ut.shrink_wrap(guess, threshold, sigma)
-    np.save(os.path.join(dir, 'support.npy'), support)
-
 def threshold_by_edge(fp: np.ndarray) -> np.ndarray:
     # threshold by left edge value
     mask = np.ones_like(fp, dtype=bool)
@@ -119,68 +74,6 @@ def get_oversample_ratio(fp: np.ndarray) -> np.ndarray:
     ]
     return np.round(oversample, 3)
 
-# copied from Mathew's code
-def Bin(Array, Binsizes):
-    odims = Array.shape
-    ndims = []
-    Binsizes = list(Binsizes)
-    if len(Binsizes) < len(odims):
-        while len(odims) > len(Binsizes):
-            Binsizes.append(1)
-
-    #playing interger math tricks
-    n = 0
-    for d in odims:
-        ndims.append(int(d / Binsizes[n] * Binsizes[n]))
-        n += 1
-
-    def getindex(n, binsize, start, ndim):
-        ind = []
-        for i in range(ndim):
-            if i < n:
-                ind.append(slice(0, ndims[i]))
-            elif i == n:
-                #playing integer math games....
-                ind.append(slice(a1, odims[i] // binsize * binsize, binsize))
-            else:
-                ind.append(slice(0, odims[i]))
-        return ind
-
-    # Bin each dim separately using a an indexing trick
-    arrs = [Array.copy()]
-    n = 0
-    for b in Binsizes:
-        for a1 in range(b):
-            if a1 == 0:
-                arrs.append(arrs[0][tuple(getindex(n, b, a1, len(odims)))])
-            else:
-                arrs[1] += arrs[0][tuple(getindex(n, b, a1, len(odims)))]
-        n += 1
-        del arrs[0]
-
-    return arrs[0]
-
-def crop_pad(IN, dim):
-    [h, w, t] = IN.shape
-    output = np.zeros(dim, dtype=IN.dtype)
-    if h >= dim[0] and w >= dim[1] and t >= dim[2]:
-        output = IN[h // 2 - dim[0] // 2:h // 2 + dim[0] // 2,
-                    w // 2 - dim[1] // 2:w // 2 + dim[1] // 2,
-                    t // 2 - dim[2] // 2:t // 2 + dim[2] // 2]
-    else:
-        ps = np.max([[h, w, t], list(dim)], axis=0)
-        lb = ps // 2 - (np.floor(np.array([h, w, t]) / 2)).astype('int32')
-        ub = ps // 2 + (np.ceil(np.array([h, w, t]) / 2)).astype('int32')
-
-        IN_pad = np.zeros(ps, dtype=IN.dtype)
-        IN_pad[lb[0]:ub[0], lb[1]:ub[1], lb[2]:ub[2]] = IN
-
-        lb = ps // 2 - (np.floor(np.array(dim) / 2)).astype('int32')
-        ub = ps // 2 + (np.ceil(np.array(dim) / 2)).astype('int32')
-
-        output = IN_pad[lb[0]:ub[0], lb[1]:ub[1], lb[2]:ub[2]]
-    return output
-
 def Resize(IN, dim):
     ft = np.fft.fftshift(np.fft.fftn(IN)) / np.prod(IN.shape)
     ft_resize = crop_pad(ft, dim)
@@ -203,9 +96,11 @@ def match_oversample_diff(
     change = [np.round(f / t).astype('int32') for f, t in zip(fr, to)]
     change = [np.max([1, c]) for c in change]
 
-    diff = Bin(diff, change)
+    diff = ut.binning(diff, change)
     # crop diff to match output shape
-    output = crop_pad(diff, shape)
+    pad_value = np.array(shape) // 2 - np.array(diff.shape) // 2
+    pad = [[pad_value[0], pad_value[0]], [pad_value[1], pad_value[1]], [pad_value[2], pad_value[2]]]    
+    output = ut.adjust_dimensions(diff, pad)
     return output, diff.shape
 
 def shift_com(amp, phi):
@@ -214,8 +109,6 @@ def shift_com(amp, phi):
 
     h,w,t = 64,64,64
     coms = com(amp)
-    if (np.isnan(coms[0]) or np.isnan(coms[1]) or np.isnan(coms[2])):
-        return 0
     deltas = (int(round(h / 2 - coms[0])), int(round(w / 2 - coms[1])),
               int(round(t / 2 - coms[2])))
     amp_shift = shift(amp, shift=deltas, mode='wrap')
@@ -292,3 +185,48 @@ def _fourier_transform(input):
                                  axes=[-3, -2, -1])
     Fr = K.permute_dimensions(perm_Fr, pattern=[1, 2, 3, 4, 0])
     return Fr
+
+def run_AI(data, threshold, sigma, dir):
+    print('AI guess')
+
+    print('original data shape', data.shape)
+    # prepare data to make the oversampling ratio ~3
+    wos = 3.0
+    orig_os = get_oversample_ratio(data)
+    # match oversampling to wos
+    wanted_os = [wos, wos, wos]
+    # match diff os
+    new_data, inshape = match_oversample_diff(data, orig_os, wanted_os)
+    print('processed data shape', new_data.shape)
+    new_data = new_data[np.newaxis]
+
+    # load trained network
+    model = load_model('./cohere/controller/trained_model.hdf5',custom_objects={'loss_comb2_scale': loss_comb2_scale})
+    print('successfully load the model')
+
+    # get the outputs from amplitude and phase layers
+    amp_layer_model = Model(inputs=model.input,
+                            outputs=model.get_layer('amp').output)
+    ph_layer_model = Model(inputs=model.input,
+                        outputs=model.get_layer('phi').output)
+
+    preds_amp = amp_layer_model.predict(new_data, verbose=1)
+
+    preds_phi = ph_layer_model.predict(new_data, verbose=1)
+
+    preds_amp, preds_phi = post_process(preds_amp[0,...,0],
+                                    preds_phi[0,...,0],
+                                    th=0.1,
+                                    uw=0)
+
+    pred_obj = preds_amp * np.exp(1j * preds_phi)
+    
+    # match object size with the input data
+    pred_obj = Resize(pred_obj, inshape)
+    guess = crop_pad(pred_obj, data.shape)
+    print('initial guess shape', guess.shape)
+
+    np.save(os.path.join(dir, 'image.npy'), guess)
+
+    support = ut.shrink_wrap(guess, threshold, sigma)
+    np.save(os.path.join(dir, 'support.npy'), support)
