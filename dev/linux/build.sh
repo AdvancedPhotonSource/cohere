b5348cde38a594c580cb642c8f9915f3bcf89a10
Barbara Frosik <bfrosik@anl.gov>
2021-11-08 16:14:03 -0600

    Delete build.sh

--- dev/linux/build.sh
+++
@@ -1,7 +0,0 @@
-python setup.py install  --prefix=$PREFIX
-
-mkdir -p "$PREFIX/etc/conda/activate.d"
-cp cohere_activate.sh $PREFIX/etc/conda/activate.d
-
-mkdir -p "$PREFIX/etc/conda/deactivate.d"
-cp cohere_deactivate.sh $PREFIX/etc/conda/deactivate.d
