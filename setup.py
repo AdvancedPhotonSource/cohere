06c9758ad37043559b66e58fc26c26fc95d2377c
Barbara Frosik <bfrosik@anl.gov>
2022-03-07 10:08:09 -0600

    Updated version

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setuptools.setup(
       author = 'Barbara Frosik, Ross Harder',
       author_email = 'bfrosik@anl.gov',
       url='https://github.com/advancedPhotonSource/cohere',
-      version='2.0',
+      version='2.1',
       packages=setuptools.find_packages()
 )
