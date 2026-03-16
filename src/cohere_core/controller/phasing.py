1ee9ce4ce57f1742c1531a5a387b96711d3a3439
bfrosik <bfrosik@anl.gov>
2026-03-16 15:56:12 -0500

    modify live trigger not to center the image, sync with beamlines

--- src/cohere_core/controller/phasing.py
+++ src/cohere_core/controller/phasing.py
@@ -486,11 +486,12 @@ class Rec:
         print(f'------iter {self.iter}   error {self.errs[-1]}')
 
     def live_operation(self):
-        self.ds_image, self.support = dvut.center_sync(self.ds_image, self.support, False)
-        # ctr = self.dims[0]//2
-        # slice = np.s_[ctr, :, :]
-        ctr = self.dims[2] // 2
-        slice = np.s_[:,:,ctr]
+        # self.ds_image, self.support = dvut.center_sync(self.ds_image, self.support, False)
+        # ctr = self.dims[2] // 2
+        # slice = np.s_[:,:,ctr]
+        # not centering image, but need to find the slice
+        com = devlib.center_of_mass(self.ds_image)
+        slice = np.s_[:, :, int(com[2])]
         title = f"Iteration: {self.iter}/{self.iter_no}\nError: {self.errs[-1]}"
         self.viewer.update_singlepeak(self.ds_image[slice], self.errs, self.support[slice], title)
 
