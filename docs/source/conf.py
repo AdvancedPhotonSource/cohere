4d0a2266f3fd42d13d68fc1469e93f0bd4f287af
Barbara Frosik <bfrosik@anl.gov>
2026-09-21 17:02:28 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, Barbara Frosik, Ross Harder'
 author = 'Barbara Frosik, Ross Harder'
 
 # The full version, including alpha/beta/rc tags
-release = '4.5.3'
+release = '4.5.4'
 
 # -- General configuration ---------------------------------------------------
 
